1e67fb6f624c56754485e8ec82305f534a9b80ac
Sumudu Tharanga <ktharangasumudu@gmail.com>
2023-11-14 16:20:35 +0530

    Added header, mega menu, mobile header, slider, and home page product display.
    Header: Improved site navigation with a customizable header.
    Mega Menu: Enhanced user experience with a feature-rich mega menu.
    Mobile Header: Optimized header for mobile responsiveness.
    Slider: Added a dynamic slider component for engaging content display.
    Home Page Product: Improved product visibility on the home page.

---
+++ script.js
@@ -0,0 +1,54 @@
+//copy Menu for mobile
+
+function copyMenu(){
+    //copy inside .dpt-cat to .deparment
+    var dptCategory=document.querySelector('.dpt-cat');
+    var dptPalace=document.querySelector('.deparments');
+    dptPalace.innerHTML=dptCategory.innerHTML;
+
+    //copy inside nav to nav
+    var mainNav=document.querySelector('.header-nav nav');
+    var navPlace=document.querySelector('.off-canvas nav')
+    navPlace.innerHTML=mainNav.innerHTML;
+
+    //Copy .header-top .wrapper to .thetop-nav
+    var topNav=document.querySelector('.header-top .wrapper');
+    var topPlace=document.querySelector('.off-canvas .thetop-nav')
+    topPlace.innerHTML=topNav.innerHTML;
+
+}
+copyMenu();
+//show moibile menu
+const menuButton=document.querySelector('.trigger'),
+    closeButton=document.querySelector('.t-close'),
+    addclass=document.querySelector('.site')
+menuButton.addEventListener('click',function(){
+    addclass.classList.toggle('showmenu')
+})
+closeButton.addEventListener('click',function(){
+    addclass.classList.remove('showmenu')
+})
+
+
+//show sub menu on mobile
+
+const submenu=document.querySelectorAll('.has-child .icon-small');
+submenu.forEach((menu)=>menu.addEventListener('click',toggle));
+
+function toggle(e){
+    e.preventDefault();
+    submenu.forEach((item)=>item != this ? item.closest('.has-child').classList.remove('expand'):null);
+    if(this.closest('.has-child').classList != 'expand');
+    this.closest('.has-child').classList.toggle('expand');
+
+
+}
+//slider
+const swiper = new Swiper('.swiper', {
+    loop: true,
+  
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+  });
